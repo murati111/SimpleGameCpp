7e34c1b2774c28f186a0b2b74c414dc0a0e9c943
murati111 <murati111taisyo@gmail.com>
2019-10-30 01:27:29 +0900

    【Add】プロジェクトファイル追加

---
+++ Source/SimpleGameCpp/GameCameraActor.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/CameraActor.h"
+#include "GameCameraActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable)
+class SIMPLEGAMECPP_API AGameCameraActor : public ACameraActor
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/SimpleGameCpp/Private/PlayerPawn.cpp
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Public/PlayerPawn.h"
+#include "UObject/ConstructorHelpers.h"
+#include "Engine/StaticMesh.h"
+// Sets default values
+APlayerPawn::APlayerPawn(const FObjectInitializer& ObjectInitializer):Super(ObjectInitializer)
+{
+	StaticMeshComponent = ObjectInitializer.CreateAbstractDefaultSubobject<UStaticMeshComponent>(this, TEXT("StaticMesh"));
+	
+	if (StaticMeshComponent != nullptr)
+	{
+		RootComponent = StaticMeshComponent;
+	}
+	static ConstructorHelpers::FObjectFinder<UStaticMesh>PlayerMeshObj(TEXT("/Game/SM_Player.SM_Player"));
+
+	if (PlayerMeshObj.Succeeded())
+	{
+		StaticMeshComponent->SetStaticMesh(PlayerMeshObj.Object);
+	}
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void APlayerPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APlayerPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void APlayerPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
